19a576e1a9b6deae2bc376d8dabc2721a98b3b01
Your Name <manas10@live.com>
2021-09-02 13:15:33 +0530

    merge without extra space

---
+++ Array/ Merge Without Extra Space/code.cpp
@@ -0,0 +1,54 @@
+//https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays5135/1#
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	void merge(int arr1[], int arr2[], int n, int m)
+    {
+        int i = 0, j = 0, k = n - 1;
+       
+        // Untill i less than equal to k
+        // or j is less tha m
+        while (i <= k && j < m) 
+            if (arr1[i] < arr2[j]) i++;
+            else swap(arr2[j++], arr1[k--]);
+        
+       
+        // Sort first array
+        sort(arr1, arr1 + n);
+       
+        // Sort second array
+        sort(arr2, arr2 + m);
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, i;
+        cin >> n >> m;
+        int arr1[n], arr2[m];
+        for (i = 0; i < n; i++) {
+            cin >> arr1[i];
+        }
+        for (i = 0; i < m; i++) {
+            cin >> arr2[i];
+        }
+        Solution ob;
+        ob.merge(arr1, arr2, n, m);
+        for (i = 0; i < n; i++) {
+            cout << arr1[i] << " ";
+        }
+        for (i = 0; i < m; i++) {
+            cout << arr2[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
