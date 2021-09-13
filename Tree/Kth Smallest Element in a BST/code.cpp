6aca006b6e0956deb966e4c776c64df3c0fe231d
Your Name <manas10@live.com>
2021-09-13 18:33:39 +0530

    added base structure definition to kth smallest lement

--- Tree/Kth Smallest Element in a BST/code.cpp
+++ Tree/Kth Smallest Element in a BST/code.cpp
@@ -3,6 +3,17 @@ src = https://leetcode.com/problems/kth-smallest-element-in-a-bst/
 concept = in order traversal
 */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     int ans = -1;
