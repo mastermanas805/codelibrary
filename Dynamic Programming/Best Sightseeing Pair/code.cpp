e62ac2820b24a13f2d28aa70c7b84311473d5966
Manas Srivastava <mastermanas805@gmail.com>
2022-04-01 20:16:23 +0530

    best-sightseeing-pair

---
+++ Dynamic Programming/Best Sightseeing Pair/code.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/best-sightseeing-pair/
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& A) {
+        int res = 0, cur = 0;
+        for (int a: A) {
+            res = max(res, cur + a);
+            cur = max(cur, a) - 1;
+        }
+        return res;
+    }
+};
