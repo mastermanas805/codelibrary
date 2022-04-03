9321047df2208d0feaeee47bb77f68be0d7737b0
Manas Srivastava <mastermanas805@gmail.com>
2022-04-03 09:49:24 +0530

    Maximum Candies Allocated to K Children

---
+++ Binary Search/Maximum Candies Allocated to K Children/code.cpp
@@ -0,0 +1,19 @@
+//  Maximum Candies Allocated to K Children
+ // https://leetcode.com/problems/maximum-candies-allocated-to-k-children/
+
+class Solution {
+public:
+    int maximumCandies(vector<int>& A, long long k) {
+        int left = 0, right = 1e7;
+        while (left < right) {
+            long sum = 0, mid = (left + right + 1) / 2;
+            for (int& a : A) sum += a / mid;
+            
+            if (k > sum)
+                right = mid - 1;
+            else
+                left = mid;
+        }
+        return left;
+    }
+};
