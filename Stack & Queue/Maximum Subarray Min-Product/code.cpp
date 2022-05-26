ede58b2b4cdca7f0f2aa23095d0606ac09dc8dfe
Manas Srivastava <mastermanas805@gmail.com>
2022-05-26 09:19:19 +0530

    maxsumminProduct

--- Stack & Queue/Maximum Subarray Min-Product/code.cpp
+++ Stack & Queue/Maximum Subarray Min-Product/code.cpp
@@ -1,55 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
 #define mod 1000000007
 class Solution {
 public:
-    
-    
-    int moduloMultiplication(int a, int b)
-    {
-        int res = 0; // Initialize result
-
-        // Update a if it is more than
-        // or equal to mod
-        a %= mod;
-
-        while (b)
-        {
-            // If b is odd, add a with result
-            if (b & 1)
-                res = (res + a) % mod;
-
-            // Here we assume that doing 2*a
-            // doesn't cause overflow
-            a = (2 * a) % mod;
-
-            b >>= 1; // b = b / 2
-        }
-
-        return res;
-    }
-    
-    
-    int maxSumMinProduct(vector<int>& nums) {
-        nums.push_back(0);
-        stack<int> s;
-        int ret = 0, n = nums.size();
-        int a[n], i = 0, top, x;
-        a[0] = nums[i];
-        for(int i=1; i<n; i++)  a[i] = (a[i-1] + nums[i]%mod)%mod;
-        
-        while(i<n)
-        {
-            if(s.empty() || nums[i] > nums[s.top()]) s.push(i++);
-            
-            else
-            {
-
-                top = s.top();
-                s.pop();
-                x = moduloMultiplication(nums[top], s.empty()? a[i-1]: ( a[i-1] - a[s.top()]) );
-                ret = max(ret, x);
+    int maxSumMinProduct(vector<int>& n) {
+        long res = 0;
+        vector<long> dp(n.size() + 1), st;
+        for (int i = 0; i < n.size(); ++i)
+           dp[i + 1] = dp[i] + n[i];
+        for (int i = 0; i <= n.size(); ++i) {
+            while (!st.empty() && (i == n.size() || n[st.back()] > n[i])) {
+                int j = st.back();
+                st.pop_back();
+                res = max(res, n[j] * (dp[i] - dp[st.empty() ? 0 : st.back() + 1]));
             }
+            st.push_back(i);
         }
-        
-        return ret;
+        return res % 1000000007;
     }
 };
+
+int main()
+{
+    vector<int> v = {3,1,5,6,4,2};
+    Solution s;
+    cout<<s.maxSumMinProduct(v);
+}
