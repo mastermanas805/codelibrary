03944d8846bda720b772a2eddc5220c5b6aee670
Manas Srivastava <mastermanas805@gmail.com>
2022-05-26 08:51:58 +0530

    template

--- template.cpp
+++ template.cpp
@@ -7,3 +7,9 @@ using namespace std;
 #define mp make_pair
 #define pii pair<int,int>
 int bitcount(int x){ int count = 0; while(x) { count++; x >>= 1; } return count; }
+
+int32_t main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+}
