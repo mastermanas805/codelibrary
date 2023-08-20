4b1d5a9e74e14a01ec8de3d1b374f49b7cb27322
Manas Srivastava <40285830+mastermanas805@users.noreply.github.com>
2023-08-20 18:37:22 +0530

    recursive solution

--- Dynamic_Programming/edit-distance/2d.cpp
+++ Dynamic_Programming/edit-distance/2d.cpp
@@ -1,7 +1,40 @@
 // https://leetcode.com/problems/edit-distance
 #include<bits/stdc++.h>
 using namespace std;
-class Solution {
+
+// Time Complexity: O(3^m)
+// Space Complexity: O(m)
+class RecursiveSolution {
+public:
+    int count(string word1, string word2, int i, int j){
+        if(i < 0)
+            return j + 1;
+
+        if(j < 0)
+            return i + 1;
+
+        if(word1[i] == word2[j])
+            return count(word1, word2, i-1, j-1);
+
+        // insert i , j-1
+        // delete i-1, j
+        // update i-1,j-1 
+        
+        return 1 + min({    count(word1, word2, i, j-1),  
+                            count(word1, word2, i - 1, j), 
+                            count(word1, word2, i-1, j-1)  });
+    }
+
+    int minDistance(string word1, string word2){
+        return count(word1, word2, word1.length() - 1, word2.length() - 1);
+    }
+
+};
+
+
+// Time Complexity: O(n*m)
+// Space Complexity: O(n*m)
+class DPSolution {
 public:
     int minDistance(string word1, string word2) {
         int n = word1.length(), m = word2.length();
